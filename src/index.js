e2cd73299595c8f41706ebf50dec43435e6b2ad7
DavidDGamble <daviddgamble@gmail.com>
2022-11-28 11:59:03 -0800

    Add falling animation to nut.
    Co-authored by bodwood <bodiewood@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 let squirrel = document.getElementById("squirrel");
-//let nut = document.getElementById("nut");
+// let nut = document.getElementById("nut");
 let both = 0;  //if users clicks both keys at the same time
 let interval;
 
 function moveLeft(){
   let left = parseInt(window.getComputedStyle(squirrel).getPropertyValue("left"));
-  // if(left > 0){
+  if(left > 0){
     squirrel.style.left = left - 2 + "px";
-  //}
+  }
 }
 
 function moveRight(){
   let left = parseInt(window.getComputedStyle(squirrel).getPropertyValue("left"));
-  //if(left < 550){
+  if(left < 550){
     squirrel.style.left = left + 2 + "px";
-  //}
+  }
 }
 
 document.addEventListener("keydown", event => {
