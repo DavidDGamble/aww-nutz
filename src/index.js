c07ae71cfd681cf760bb9ba066a98a5bd8c3ca37
DavidDGamble <daviddgamble@gmail.com>
2022-11-28 10:53:33 -0800

    Add code to index.js
    Co-authored by bodwood <bodiewood@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+let squirrel = document.getElementById("squirrel");
+//let nut = document.getElementById("nut");
+let both = 0;  //if users clicks both keys at the same time
+let interval;
+
+function moveLeft(){
+  let left = parseInt(window.getComputedStyle(squirrel).getPropertyValue("left"));
+  // if(left > 0){
+    squirrel.style.left = left - 2 + "px";
+  //}
+}
+
+function moveRight(){
+  let left = parseInt(window.getComputedStyle(squirrel).getPropertyValue("left"));
+  //if(left < 550){
+    squirrel.style.left = left + 2 + "px";
+  //}
+}
+
+document.addEventListener("keydown", event => {
+  if(both === 0){
+    both++;
+    if(event.key === "ArrowLeft"){
+      interval = setInterval(moveLeft, 1);
+    }
+    if(event.key === "ArrowRight"){
+      interval = setInterval(moveRight, 1);
+    }
+  }
+});
+
+document.addEventListener("keyup", event => {
+  event.preventDefault();
+  clearInterval(interval);
+  both = 0;
+});
+
