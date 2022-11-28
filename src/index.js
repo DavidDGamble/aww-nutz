6e6c19e960c3185d8824b1d53e721b036972fde3
ChrisKLoveless <chriskloveless@gmail.com>
2022-11-28 13:05:46 -0800

    add ui logic and nut emoji
    co-authored-by:brandonfenk <brandon.fenk99@gmail.com>
    co-authored-by: Lucas Mollerstuen <mollerstuen.works@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,3 +2,35 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+function makeNut() {
+	const nutElement = document.createElement("img");
+	nutElement.setAttribute("class", "nut");
+	nutElement.setAttribute("src", "https://img.icons8.com/arcade/2x/nut.png");
+	console.log(nutElement);
+
+	document.querySelector("div.game").append(nutElement);
+	const xPos = getRandomNum(0, 600);
+	nutElement.css("left", xPos + "px");
+
+	nutElement.css("top", "-100px");
+	const nutSpeed = getRandomNum(2000, 6000);
+
+	nutElement.animate({ "top": "430px" },
+		nutSpeed, "swing", makeNut);
+}
+
+function getRandomNum(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function startGame() {
+	let num = 10;
+	while (num > 0) {
+		makeNut();
+		num--;
+	}
+}
+window.addEventListener("load", function () {
+	document.getElementById("start").addEventListener("click", startGame);
+});
+
