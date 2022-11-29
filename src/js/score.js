3c550391d7e2ff78d29a7616ee4da6c419897700
DavidDGamble <daviddgamble@gmail.com>
2022-11-29 10:46:02 -0800

    Add gameover and high score tracker.
    co-authored-by: Chris Loveless <chriskloveless@gmail.com>

--- src/js/score.js
+++ src/js/score.js
@@ -2,7 +2,22 @@ export default class Score {
 	constructor() {
 		this.highScore = 0;
 		this.currScore = 0;
+		this.level = 0;
 	}
 
+	random() {
+		return Math.floor(Math.random() * (580 - 1 + 1)) + 1;
+	}
 
+	checkHigh() {
+		if (this.currScore > this.highScore) {
+			this.highScore = this.currScore;
+		}
+	}
+
+	checkLevel() {
+		if ((this.currScore % 5 === 0 && this.currScore <= 20)) {
+			this.level ++;
+		}
+	}
 }
